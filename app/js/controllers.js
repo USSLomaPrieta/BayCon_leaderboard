ddab1bfef1bd2e27c963c77e80ac804a847636a7
iansayre <ian.sayre@3dsystems.com>
2014-05-19 08:26:44 -0700

    first commit

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,11 +1,12 @@
 'use strict';
 
+
 /* Controllers */
 
 angular.module('myApp.controllers', [])
-  .controller('MyCtrl1', ['$scope', function($scope) {
+  .controller('Game1Ctrl', ['$scope', function($scope) {
 
   }])
-  .controller('MyCtrl2', ['$scope', function($scope) {
+  .controller('Game2Ctrl', ['$scope', function($scope) {
 
   }]);
